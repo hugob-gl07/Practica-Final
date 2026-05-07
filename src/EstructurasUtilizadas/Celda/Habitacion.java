5ad4760b87eee46e81e840236cf25a2d688ee48b
Hugo <hugobastantegl@gmail.com>
2026-05-07 22:12:24 +0200

    sesión hoy: creación de las clases Celda, Habitación, Matriz Habitación y TipoCelda

---
+++ src/EstructurasUtilizadas/Celda/Habitacion.java
@@ -0,0 +1,72 @@
+package EstructurasUtilizadas.Celda;
+/**
+ * Representa una habitación individual del juego con sus propiedades y matriz interna.
+ * Cada habitación tiene un identificador único y conexiones con otras habitaciones.
+ */
+public class Habitacion {
+    private int id;
+    private String nombre;
+    private MatrizHabitacion matriz;
+
+    public Habitacion(int id, String nombre, int columnas, int filas) {
+        this.id = id;
+        this.nombre = nombre;
+        this.matriz = new MatrizHabitacion(columnas, filas);
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Celda getCelda(int fila, int columna) {
+        return matriz.getCelda(fila, columna);
+    }
+    public void setCelda(int fila, int columna, Celda celda) {
+        matriz.setCelda(fila, columna, celda);
+    }
+    public boolean esTransitable(int fila, int columna) {
+        return matriz.esTransitable(fila, columna);
+    }
+    public boolean tieneCelda(TipoCelda tipoCelda) {
+        for (int i = 0; i < matriz.getFilas(); i++) {
+            for (int j = 0; j < matriz.getColumnas(); j++) {
+                if (matriz.getCelda(i, j).getTipo() == tipoCelda) {
+                    return true ;
+                }
+            }
+        }
+        return false ;
+    }
+    public Celda buscarcelda(TipoCelda tipoCelda) {
+        for (int i = 0; i < matriz.getFilas(); i++) {
+            for (int j = 0; j < matriz.getColumnas(); j++) {
+                if (matriz.getCelda(i, j).getTipo() == tipoCelda) {
+                    return matriz.getCelda(i,j);
+                }
+            }
+        }
+         throw new IllegalArgumentException("No se encontró una celda del tipo especificado en la habitación.");
+    }
+    public int getFila(){
+        return matriz.getFilas();
+    }
+    public int getColumna(){
+        return matriz.getColumnas();
+    }
+
+    public String toString() {
+        return "Habitacion{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", matriz=\n" + matriz.toString() +
+                '}';
+    }
+
+}
