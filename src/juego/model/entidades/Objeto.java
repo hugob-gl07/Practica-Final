7b45eab286ed46566c7dda38beee9cae71c6473e
hugob <hugo.bastante@edu.uah.es>
2026-05-09 21:00:16 +0200

    Creamos las entidades del juego

---
+++ src/juego/model/entidades/Objeto.java
@@ -0,0 +1,66 @@
+package juego.model.entidades;
+
+public class Objeto implements Comparable<Objeto>{
+    private int id;
+    private String nombre;
+    private  TipoObjeto tipo;
+    private Rareza rareza;
+    private String descripcion;
+    private String [] nombreEstadisticas;
+    private int[] valorEstadisticas;
+    private int numeroEstadisticas;
+    private String efecto;
+    private float peso;
+    public Objeto(int id, String nombre, TipoObjeto tipo , Rareza rareza) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.rareza = rareza;
+        this.nombreEstadisticas = new String[10];
+        this.valorEstadisticas = new int[10];
+        this.numeroEstadisticas = 0;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public TipoObjeto getTipo() {
+        return tipo;
+    }
+    public Rareza getRareza() {
+        return rareza;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public String getEfecto() {
+        return efecto;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public void addEstadistica(String nombre, int valor) {
+        nombreEstadisticas[numeroEstadisticas] = nombre;
+        valorEstadisticas[numeroEstadisticas] = valor;
+        numeroEstadisticas++;
+    }
+    public int getValorEstadisticas(String nombre) {
+        for (int i = 0; i < numeroEstadisticas; i++) {
+            if(nombreEstadisticas[i].equals(nombre)){
+                return valorEstadisticas[i];
+            }
+        }
+        return 0;
+    }
+    @Override
+    public String toString() {
+        return "Objeto{ id= " + id + ", nombre= " + nombre + ", tipo= " + tipo + ", rareza= " + rareza + ", descripción= " + descripcion + "}";
+    }
+    @Override
+    public int compareTo(Objeto o) {
+        return this.id - o.id;
+    }
+
+}
